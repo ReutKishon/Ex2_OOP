dbd7cb875b4f9939e97ef7cc6f50c2575369eed8
eladkeyshawn <elad.keyshawn@gmail.com>
2020-12-22 10:51:41 +0200

    add command args support

--- src/gameClient/Ex2.java
+++ src/gameClient/Ex2.java
@@ -1,4 +1,5 @@
 package gameClient;
+
 import gui.Window;
 
 
@@ -8,14 +9,28 @@ public class Ex2 {
     private static int num_level;
 
 
-    public static void main(String[] a) {
+    public static void main(String[] args) {
+
+
+        if (args.length > 0) {
+
+            try {
+                var id = Integer.parseInt(args[0]);
+                var level = Integer.parseInt(args[1]);
+                GameEntryPoint gameEntryPoint = new GameEntryPoint(level, id);
+                gameEntryPoint.run();
+            } catch (NumberFormatException e) {
+                System.out.println("Usage java -jar program.jar [YOUR_ID] [LEVEL_NUMBER]");
+            }
+            return;
+        }
+
 
         Window window = new Window();
     }
 
 
-
-    }
+}
 
 
 
